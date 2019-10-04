4c5f0a3082956e1b7cf7554d3a51eca05330e6d5
Michael Wahl <michaelwahl9@gmail.com>
2019-10-04 15:54:05 -0500

    finish base ui

---
+++ client.js
@@ -0,0 +1,28 @@
+console.log('client.js running')
+
+$('document').ready(onReady);
+
+function onReady(){
+    $('#inputBtn').on('click', inputClick);
+}
+
+function inputClick(){
+    fName = $('#fName').val();
+    lName = $('#lName').val();
+    idNumber = $('#idNumber').val();
+    title = $('#title').val();
+    salary = $('#salary').val();
+    addToDom();
+}
+
+function addToDom(){
+    let output = `<tr>
+        <th>${fName}</th>
+        <th>${lName}</th>
+        <th>${idNumber}</th>
+        <th>${title}</th>
+        <th>$${salary}</th>
+        <th><button class="deleteBtn">Delete</button></th>
+    </tr>`
+    $('table').append(output);
+}
